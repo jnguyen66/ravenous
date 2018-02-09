c1357a689c5c1c9ca2aa7c466d19a313836f3ed9
Justin Nguyen <jvnguyen66@gmail.com>
2018-02-08 22:48:12 -0800

    Added component communication

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business'
 
-class BusinessList extends React.Component{
-  render(){
+class BusinessList extends React.Component {
+  render() {
     return (
       <div className="BusinessList">
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-</div>
-    )
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
   }
 }
 
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,8 +11,7 @@ const sortByOptions={
 
 class SearchBar extends React.Component{
   renderSortByOptions(){
-    return
-    Object.keys(sortByOptions).map(sortByOption =>{
+    return Object.keys(sortByOptions).map(sortByOption =>{
       let sortByOptionValue = sortByOptions[sortByOption];
       return <li key={sortByOptionValue}> {sortByOption}</li>;
     });
