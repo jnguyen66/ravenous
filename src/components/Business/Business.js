6aab40997e9b74691e345d2e7fc23eb1c1ae15d9
Justin Nguyen <jvnguyen66@gmail.com>
2018-02-19 15:08:51 -0800

    Added Yelp API functionality

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -7,6 +7,7 @@ class Business extends React.Component{
 
   render(){
     return (
+
       <div className="Business">
   <div className="image-container">
     <img src={this.props.business.imageSrc} alt=''/>
